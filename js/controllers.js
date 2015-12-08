883c7d4afd7ec763dd8c82d2d17b28d44e343f0a
papastepan <stepan@segments.at>
2015-12-08 13:59:56 +0100

    My first commit

---
+++ js/controllers.js
@@ -0,0 +1,52 @@
+'use strict';
+
+/* Controllers */
+
+var helloWorldControllers = angular.module('helloWorldControllers', []);
+
+
+
+helloWorldControllers.controller('MainCtrl', ['$scope', '$location', '$http',
+    function MainCtrl($scope, $location, $http) {
+        $scope.message = "Hello World";
+    }]);
+
+helloWorldControllers.controller('ShowCtrl', ['$scope', '$location', '$http',
+    function ShowCtrl($scope, $location, $http) {
+        $scope.message = "Show The World";
+    }]);
+
+helloWorldControllers.controller('CustomerCtrl', ['$scope', 
+function CustomerCtrl($scope) { 
+    $scope.customerName = "Bob's Burgers"; 
+    $scope.customerNumber = "44522";
+    
+    // add method to scope
+    $scope.changeCustomer = function(){
+      $scope.customerName = $scope.cName;
+      $scope.customerNumber = $scope.cNumber;
+    };
+}]);
+
+helloWorldControllers.controller('AddCustomerCtrl', 
+['$scope', '$location',
+  function AddCustomerCtrl($scope, $location) {
+
+
+
+helloWorldControllers.controller('AddedCustomerCtrl', 
+['$scope', '$routeParams',
+function AddedCustomerCtrl($scope, $routeParams) {
+  $scope.customerName = $routeParams.customer;
+  $scope.customerCity = $routeParams.city;
+}]);
+    $scope.submit = function(){
+    $location.path('/addedCustomer/' + $scope.cName + "/" + $scope.cCity);
+  };
+}]);
+helloWorldControllers.controller('AddedCustomerCtrl', 
+['$scope', '$routeParams',
+  function AddedCustomerCtrl($scope, $routeParams) {
+    $scope.customerName = $routeParams.customer;
+    $scope.customerCity = $routeParams.city;
+}]);
